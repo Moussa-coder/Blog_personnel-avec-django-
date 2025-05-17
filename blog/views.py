1fc638b920c028859b5da12aadfedb2e2fe7079a
Moussa-codeur <correamoussa469@gmail.com>
2025-05-17 16:32:59 +0200

    Création de mon views et formulaire

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Article
+from .forms import ArticleForm
 
 # Create your views here.
+# Lire (Liste des articles)
+def article_list(request):
+    articles = Article.objects.all().order_by('-date_creation')
+    return render(request, 'blog/article_list.html', {'articles': articles})
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, 'blog/article_detail.html', {'article': article})
+
+# Créer
+def article_create(request):
+    form = ArticleForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('article_list')
+    return render(request, 'blog/article_form.html', {'form': form})
+
+# Modifier
+def article_update(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    form = ArticleForm(request.POST or None, instance=article)
+    if form.is_valid():
+        form.save()
+        return redirect('article_list')
+    return render(request, 'blog/article_form.html', {'form': form})
+
+# Supprimer
+def article_delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == 'POST':
+        article.delete()
+        return redirect('article_list')
+    return render(request, 'blog/article_confirm_delete.html', {'article': article})
