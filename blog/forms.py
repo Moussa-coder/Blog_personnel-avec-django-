a1a51b9998aaae0d3903c18f317d1b8be5ff438c
Moussa-codeur <correamoussa469@gmail.com>
2025-05-17 16:46:14 +0200

    Création des templates

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from .models import Article
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
-        fields = ['article', 'contenu']
+        fields = ['titre', 'content']
