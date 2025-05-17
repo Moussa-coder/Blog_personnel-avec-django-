697b54c38f2f65da377fbf1ae1596c5478408725
Moussa-codeur <correamoussa469@gmail.com>
2025-05-17 16:16:41 +0200

    Création de mon model

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Article(models.Model):
-    titre = models.CharField
+    titre = models.CharField(max_length=200)
+    content = models.TextField()
+    date_creation = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.titre
